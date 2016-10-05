212eb740ebf5df72d41b08108240348b0080417b
zakhttp <zakhttp@localhost.localdomain>
2016-10-05 19:00:26 +0400

    Fix folder copy command

--- sync.sh
+++ sync.sh
@@ -1,7 +1,8 @@
 echo "copying .config/ folders";
-cp -r ~/.config/gtk-2.0/  .config/gtk-2.0/;
-cp -r ~/.config/gtk-3.0/  .config/gtk-3.0/;
-cp -r ~/.config/i3        .config/i3/;
+mkdir .config;
+cp -avr ~/.config/gtk-2.0/  .config/gtk-2.0/;
+cp -avr ~/.config/gtk-3.0/  .config/gtk-3.0/;
+cp -avr ~/.config/i3        .config/i3/;
 
 echo "copying .Xdefaults";
 cp ~/.Xdefaults .Xdefaults;
@@ -10,7 +11,7 @@ echo "copying .Xresources";
 cp ~/.Xresources .Xresources;
 
 echo "copying .vim/";
-cp -r ~/.vim .vim;
+cp -avr ~/.vim .vim;
 
 echo "copying .vimrc";
 cp ~/.vimrc .vimrc;
